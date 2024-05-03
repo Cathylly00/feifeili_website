445229234cf01182c7f0c016d79ad87f77992546
Cathylly00 <161824226+Cathylly00@users.noreply.github.com>
2024-05-03 16:04:26 +0800

    0503_ver01

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,6 +17,53 @@ $(document).ready(function () {
 });
 
 // Scroll
+
+//index-01
+document.addEventListener('DOMContentLoaded', function () {
+    const ele = document.getElementById('scroll-area-index');
+    ele.style.cursor = 'grab';
+
+    let pos = { top: 0, left: 0, x: 0, y: 0 };
+
+    const mouseDownHandler = function (e) {
+        ele.style.cursor = 'grabbing';
+        ele.style.userSelect = 'none';
+
+        pos = {
+            left: ele.scrollLeft,
+            top: ele.scrollTop,
+            // Get the current mouse position
+            x: e.clientX,
+            y: e.clientY,
+        };
+
+        document.addEventListener('mousemove', mouseMoveHandler);
+        document.addEventListener('mouseup', mouseUpHandler);
+    };
+
+    const mouseMoveHandler = function (e) {
+        // How far the mouse has been moved
+        const dx = e.clientX - pos.x;
+        const dy = e.clientY - pos.y;
+
+        // Scroll the element
+        ele.scrollTop = pos.top - dy;
+        ele.scrollLeft = pos.left - dx;
+    };
+
+    const mouseUpHandler = function () {
+        ele.style.cursor = 'grab';
+        ele.style.removeProperty('user-select');
+
+        document.removeEventListener('mousemove', mouseMoveHandler);
+        document.removeEventListener('mouseup', mouseUpHandler);
+    };
+
+    // Attach the handler
+    ele.addEventListener('mousedown', mouseDownHandler);
+});
+
+//project-01
 document.addEventListener('DOMContentLoaded', function () {
     const ele = document.getElementById('scroll-area-1');
     ele.style.cursor = 'grab';
@@ -60,7 +107,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Attach the handler
     ele.addEventListener('mousedown', mouseDownHandler);
 });
-
+//project-02
 document.addEventListener('DOMContentLoaded', function () {
     const ele = document.getElementById('scroll-area-2');
     ele.style.cursor = 'grab';
